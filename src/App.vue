41ee7b929b4fd4c10a653dd76ffb6ed54c2e25f5
“xxj435” <861479614@qq.com>
2022-02-09 22:43:35 +0800

    初始化 完成svg验证码图片切换

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,71 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="layui-container">
+      <form class="layui-form" action="">
+        <div class="layui-form-item">
+          <label class="layui-form-label">输入框</label>
+          <div class="layui-input-block">
+            <input type="text" name="title" required  lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">密码</label>
+          <div class="layui-input-block">
+            <input type="password" name="title" required  lay-verify="required" placeholder="请输入密码" autocomplete="off" class="layui-input">
+          </div>
+        </div>    <div class="layui-form-item">
+        <label class="layui-form-label">验证码</label>
+        <div class="layui-input-inline">
+          <input type="password" name="password" required lay-verify="required" placeholder="请输入验证码" autocomplete="off" class="layui-input">
+        </div>
+        <div class="layui-form-mid layui-word-aux" v-html="svgUrl" @click="getCaptcha" style="position: relative; top: -15px;">
+
+        </div>
+      </div>
+        <button type="button" class="layui-btn">点击登录</button>
+        <a href="http://www.layui.com" class="imooc-link">忘记密码?</a>
+      </form>
     </div>
-    <router-view/>
+
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import axios from 'axios'
+export default {
+  name: 'app',
+  data(){
+    return {
+      svgUrl: ''
     }
+  },
+  mounted() {
+    this.getCaptcha()
+  },
+  methods:{
+    getCaptcha(){
+      axios.get('http://127.0.0.1:4000/getCaptcha').then(res => {
+        if(res.data.code == 200){
+          this.svgUrl = res.data.data
+        }
+      })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+#app {
+  background: #f2f2f2;
+}
+.layui-container{
+  background: #ffff;
+}
+input{
+  width: 190px;
+}
+.imooc-link{
+  margin-left: 20px;
+  &:hover{
+    color: #42b983;
   }
 }
 </style>
